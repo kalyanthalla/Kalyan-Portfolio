6c9d969733358baa13ad21b84d706f7c6b9a3abb
kalyanthalla <thallasaikalyan@gmail.com>
2025-06-20 09:10:46 +0530

    deleted not required icons

--- assets/assets.js
+++ assets/assets.js
@@ -1,25 +1,7 @@
-import code_icon from './code-icon.png';
-import code_icon_dark from './code-icon-dark.png';
-import edu_icon from './edu-icon.png';
-import edu_icon_dark from './edu-icon-dark.png';
-import project_icon from './project-icon.png';
-import project_icon_dark from './project-icon-dark.png';
 import html_icon from './html_icon.svg';
 import node_icon from './node_icon.svg';
-import vscode from './vscode.png';
-import firebase from './firebase.png';
-import figma from './figma.png';
-import git from './git.png';
-import mongodb from './mongodb.png';
-import right_arrow_white from './right-arrow-white.png';
 import logo from './logo.png';
 import logo_dark from './logo_dark.png';
-import mail_icon from './mail_icon.png';
-import mail_icon_dark from './mail_icon_dark.png';
-import github_icon from './github_icon.svg';
-import github_icon_dark from './github_icon_dark.svg';
-import linkedin_icon from './linkedin_icon.svg'
-import linkedin_icon_dark from './linkedin_icon_dark.svg'
 import profile_image from './profile_image.jpeg';
 import download_icon from './download_icon.svg';
 import download_icon_dark from './download_icon_dark.svg';
@@ -27,44 +9,17 @@ import moon_icon from './moon_icon.svg';
 import moon_icon_fill from './moon_icon_fill.svg';
 import sun_icon from './sun_icon.svg';
 import sun_icon_fill from './sun_icon_fill.svg';
-import arrow_icon from './arrow-icon.png';
-import arrow_icon_dark from './arrow-icon-dark.png';
 import menu_icon from './menu_icon.svg';
 import menu_icon_dark from './menu_icon_dark.svg';
 import close_icon from './close_icon.svg';
 import close_icon_dark from './close_icon_dark.svg';
-import web_icon from './web-icon.png';
-import mobile_icon from './mobile-icon.png';
-import ui_icon from './ui-icon.png';
-import graphics_icon from './graphics-icon.png';
-import right_arrow from './right-arrow.png';
-import send_icon from './send-icon.png';
-import right_arrow_bold from './right-arrow-bold.png';
-import right_arrow_bold_dark from './right-arrow-bold-dark.png';
 
 export const assets = {
-    code_icon,
-    code_icon_dark,
-    edu_icon,
-    edu_icon_dark,
-    project_icon,
-    project_icon_dark,
+    
     html_icon,
     node_icon,
-    vscode,
-    firebase,
-    figma,
-    git,
-    mongodb,
-    right_arrow_white,
     logo,
     logo_dark,
-    mail_icon,
-    mail_icon_dark,
-    github_icon,
-    github_icon_dark,
-    linkedin_icon,
-    linkedin_icon_dark,
     profile_image,
     download_icon,
     download_icon_dark,
@@ -72,20 +27,10 @@ export const assets = {
     moon_icon_fill,
     sun_icon,
     sun_icon_fill,
-    arrow_icon,
-    arrow_icon_dark,
     menu_icon,
     menu_icon_dark,
     close_icon,
     close_icon_dark,
-    web_icon,
-    mobile_icon,
-    ui_icon,
-    graphics_icon,
-    right_arrow,
-    send_icon,
-    right_arrow_bold,
-    right_arrow_bold_dark
 };
 
 export const workData = [
